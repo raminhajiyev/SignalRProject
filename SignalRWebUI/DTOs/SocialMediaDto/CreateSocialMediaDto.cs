340d6ef44db801e56dc4c0121df3e0bd735abda5
Ramin Haciyev <ramin.haciyev.2014@gmail.com>
2024-04-03 14:59:00 +0200

    Social Media and Testimonial

---
+++ SignalRWebUI/DTOs/SocialMediaDto/CreateSocialMediaDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRWebUI.DTOs.SocialMediaDto
+{
+    public class CreateSocialMediaDto
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
